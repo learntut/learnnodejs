8af4d9bef2b9f4d13bdf3147cbc263fe58f00b29
Shyam S <shyam.s@thinkpalm.com>
2017-06-16 11:10:30 +0530

    create DB

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var exphbs = require('express-handlebars')
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -10,13 +11,33 @@ var options = { dotfiles: 'ignore', etag: false,
                 extensions: ['htm', 'html'],
                 index: false};
 
+var url = "mongodb://localhost:12701/mydb";
+
 app.use(express.static(path.join(__dirname, 'public') , options  ));
 
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
 
 app.get('/', function(req, res)
 {
     res.render('index');   // this is the important part
 });
+
+app.get('/movies',function(req,res){
+
+});
+
+app.get('/addmovie',function(req,res){
+
+});
+
+app.get('/editmovie',function(req,res){
+
+});
+
 
 app.listen(app.get('port'),  function () {
     console.log('Hello express started on http://localhost:' +
